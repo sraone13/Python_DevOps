30790905cccf5e027129c2c5a1ecd4d28e12a17a
unknown <sravan@gmail.com>
2025-04-30 14:48:15 +0530

    Remove sensitive data

--- Day-14/list_projects.py
+++ Day-14/list_projects.py
@@ -5,8 +5,7 @@ import json
 
 url = "https://sravanpeddapally.atlassian.net/rest/api/3/project"
 
-API_TOKEN = "ATATT3xFfGF0jGHDsE2HdfYiR6NpNcivJs9YtZWqrqg8f2MIJJ9V28xslriX7BmMyahlm1FtV_0OWdHyp5Z_Gei8D5gK3Md5OKqB_TDiNCAvGUe9zUd5sweUbosrAQH2nUDcRTwSDZz3cJv96KArXlsYl9sM-UhVfczknpE13WTA5ORt9435IDc=CDF06949"
-
+API_TOKEN = ""
 auth = HTTPBasicAuth("sravanpeddapally@gmail.com", API_TOKEN)
 
 headers = {
